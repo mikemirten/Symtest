4b3c5bc205fc4e19955e7c17aaea8421a85b32c4
mikemirten <mikemirten@gmail.com>
2014-12-05 18:31:28 -0500

    WallPostForm added

---
+++ src/WallPostBundle/Form/WallPostForm.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace WallPostBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class WallPostForm extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('title', 'text', [
+				'label' => 'Title'
+			])
+            ->add('author', 'text', [
+				'label'    => 'Author',
+				'required' => false
+			])
+            ->add('body', 'textarea', [
+				'label' => 'Message'
+			])
+			->add('submit', 'submit');
+    }
+    
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults([
+			'method'     => 'post',
+            'data_class' => 'WallPostBundle\Entity\WallPost'
+        ]);
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'wallpost_post';
+    }
+}
