6abe31deac7f78b41580dfa0203a58dbe06a66b7
mikemirten <mikemirten@gmail.com>
2014-12-05 20:44:36 -0500

    Some tests added for the WallPostController

--- src/WallPostBundle/Tests/Controller/WallPostControllerTest.php
+++ src/WallPostBundle/Tests/Controller/WallPostControllerTest.php
@@ -3,6 +3,7 @@
 namespace WallPostBundle\Tests\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\DomCrawler\Crawler;
 
 class WallPostControllerTest extends WebTestCase
 {
@@ -11,6 +12,11 @@ class WallPostControllerTest extends WebTestCase
         $client = static::createClient();
 
         $crawler = $client->request('GET', '/');
+		
+		$form = $crawler->filter('form[name="wallpost_post"]');
+		$this->assertSame(1, $form->count());
+		
+		$this->postFormElementsTest($form);
     }
 
     public function testCreate()
@@ -18,6 +24,32 @@ class WallPostControllerTest extends WebTestCase
         $client = static::createClient();
 
         $crawler = $client->request('GET', '/create');
+		
+		$form = $crawler->filter('form[name="wallpost_post"]');
+		$this->assertSame(1, $form->count());
+		
+		$this->postFormElementsTest($form);
+		
+		$token = $crawler->filter('input[name="wallpost_post[_token]"]')->first()->attr('value');
+		
+		$client->request('post', '/create', [
+			'wallpost_post' => [
+				'title'  => 'Testing Controllers in Symfony2',
+				'author' => 'author',
+				'body'   => 'body',
+				'_token' => $token
+			]
+		]);
+		
+		$this->assertTrue($client->getResponse()->isRedirect('/'));
     }
+	
+	protected function postFormElementsTest(Crawler $form) {
+		$this->assertSame(1, $form->filter('input[name="wallpost_post[title]"]')->count());
+		$this->assertSame(1, $form->filter('input[name="wallpost_post[author]"]')->count());
+		$this->assertSame(1, $form->filter('textarea[name="wallpost_post[body]"]')->count());
+		$this->assertSame(1, $form->filter('button[type="submit"]')->count());
+		$this->assertSame(1, $form->filter('input[name="wallpost_post[_token]"]')->count());
+	}
 
 }
